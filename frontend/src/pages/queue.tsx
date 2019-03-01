680f34b6358b7c11a830126535dd79bdbb3a28d7
Rasmus Eskola <fruitiex@gmail.com>
2019-03-01 17:33:58 +0200

    fix duration maybe

--- frontend/src/pages/queue.tsx
+++ frontend/src/pages/queue.tsx
@@ -14,9 +14,15 @@ interface ComponentState {
 }
 
 const msToTime = (duration: number) => {
-  const seconds = String((duration / 1000) % 60).padStart(2, "0")
-  const minutes = String((duration / (1000 * 60)) % 60).padStart(2, "0")
-  const hours = String((duration / (1000 * 60 * 60)) % 24).padStart(2, "0")
+  const seconds = String(Math.floor((duration / 1000) % 60)).padStart(2, "0")
+  const minutes = String(Math.floor(duration / (1000 * 60)) % 60).padStart(
+    2,
+    "0"
+  )
+  const hours = String(Math.floor(duration / (1000 * 60 * 60)) % 24).padStart(
+    2,
+    "0"
+  )
 
   return hours + ":" + minutes + ":" + seconds
 }
