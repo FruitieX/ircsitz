d81b7c998c784e6f9d973315dc2de58cc493a9ce
Rasmus Eskola <fruitiex@gmail.com>
2019-03-01 17:17:32 +0200

    increase max song duration

--- backend/config.ts
+++ backend/config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   youtube: {
     apiKey: 'AIzaSyBi8SM9GfJyr_xOY38ec2EJ4Y6w6-xVjdo',
   },
-  maxSongDuration: 480000, // 8 minutes
+  maxSongDuration: 16 * 60 * 1000, // 16 minutes
   noDuplicates: true,
   host: 'http://player.fruitiex.org',
   apiRoot: 'http://player.fruitiex.org/api/v1',
